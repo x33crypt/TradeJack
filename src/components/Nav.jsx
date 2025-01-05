08dc17f3505561161719ba11732b808252d390dd
br3nst3rm <adeleke21l4l@gmail.com>
2025-01-05 14:32:37 +0100

    ccc

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,10 +8,10 @@ const Nav = () => {
   return (
     <div className="z-50 px-[5%] bg-[rgb(17,17,22)] lg:h-[100px] h-[90px] flex items-center justify-between fixed top-0 right-0 left-0">
       <div className="flex items-center gap-[5px]">
-        <GiCardExchange className="lg:text-[25px] text-[24px] text-tradeGreen" />
-        <p className=" lg:text-[25px] text-[22px] font-[700] text-tradeGreen">
+        <GiCardExchange className="lg:text-[25px] text-[25px] text-tradeGreen" />
+        <p className=" lg:text-[25px] text-[24px] font-[700] text-tradeGreen">
           Trade
-          <small className="lg:text-[25px] text-[22px] font-[700] text-white">
+          <small className="lg:text-[25px] text-[24px] font-[700] text-white">
             Jack
           </small>
         </p>
@@ -31,7 +31,7 @@ const Nav = () => {
         </p>
       </div>
       <div>
-        <div className="flex items-center gap-[10px] bg-white lg:px-[20px] px-[15px] py-[8px] rounded-[8px]">
+        <div className="flex items-center gap-[10px] bg-tradeGreen lg:px-[20px] px-[15px] py-[8px] rounded-[8px]">
           <p className="text-black lg:text-[16px] text-[16px] font-[600]">
             Get Started
           </p>
