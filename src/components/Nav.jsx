fe5ab980c5f48a8d2d1d662e0bd6ab75518f314f
br3nst3rm <adeleke21l4l@gmail.com>
2025-01-05 14:29:13 +0100

    dd

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,7 +6,7 @@ import { CgMenuRight } from "react-icons/cg";
 
 const Nav = () => {
   return (
-    <div className="z-50 px-[5%] bg-[rgb(17,17,22)] lg:h-[100px] h-[80px] flex items-center justify-between fixed top-0 right-0 left-0">
+    <div className="z-50 px-[5%] bg-[rgb(17,17,22)] lg:h-[100px] h-[90px] flex items-center justify-between fixed top-0 right-0 left-0">
       <div className="flex items-center gap-[5px]">
         <GiCardExchange className="lg:text-[25px] text-[24px] text-tradeGreen" />
         <p className=" lg:text-[25px] text-[22px] font-[700] text-tradeGreen">
@@ -32,14 +32,14 @@ const Nav = () => {
       </div>
       <div>
         <div className="flex items-center gap-[10px] bg-white lg:px-[20px] px-[15px] py-[8px] rounded-[8px]">
-          <p className="text-black lg:text-[16px] text-[15px] font-[600]">
+          <p className="text-black lg:text-[16px] text-[16px] font-[600]">
             Get Started
           </p>
           {/* <MdArrowOutward className="text-black text-[17px] font-bold" /> */}
         </div>
       </div>
       <div className="lg:hidden flex">
-        <CgMenuRight className="text-neutral-400 text-[24px]" />
+        <CgMenuRight className="text-neutral-400 text-[25px]" />
       </div>
     </div>
   );
