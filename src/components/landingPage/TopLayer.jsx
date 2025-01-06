7ab99ad54957c9a66d417fee26ef5fdff1d328de
br3nst3rm <adeleke21l4l@gmail.com>
2025-01-06 01:40:37 +0100

    n

--- src/components/landingPage/TopLayer.jsx
+++ src/components/landingPage/TopLayer.jsx
@@ -2,19 +2,27 @@ import React from "react";
 
 const TopLayer = () => {
   return (
-    <div className="lg:pt-[40px] pt-[80px] lg:pb-[100px] pb-[120px] flex flex-col px-[5%]">
-      <div className="flex flex-col justify-center items-center">
-        <p className="lg:text-[90px]  text-[60px] text-center text-white font-[500] lg:leading-[100px] leading-[70px] lg:w-[800px]">
-          Trade smarter, safer, and faster
-        </p>
-        <p className="mt-[25px] lg:w-[700px] font-[500] lg:text-[19px] text-[17px] text-center text-neutral-400">
-          Empowering global transactions with unmatched security, transparency,
-          and ease. Explore a marketplace designed for seamless trades and
-          profitable opportunities.
-        </p>
-        <div className="mt-[30px] lg:px-[30px] py-[10px] w-[100%] border border-tradeAsh bg-tradeGreen rounded-[8px] flex justify-center">
-          <p className="font-[600] text-black">Get Started Now</p>
+    <div className=" lg:pt-[40px] pt-[50px] lg:pb-[100px] pb-[80px] px-[5%] flex ">
+      <div className="flex">
+        <div className="flex-1 ">
+          <p className="mt-[10px] lg:text-[70px] text-[45px] text-white font-[500]  lg:leading-[80px] leading-[55px]">
+            Trade Smarter, Safer, and Faster
+          </p>
+          <p className="mt-[20px] text-neutral-400 lg:text-[18px] lg:leading-[30px] leading-[25px]">
+            Empowering global transactions with unmatched security,
+            transparency, and ease. Explore a marketplace designed for seamless
+            trades and profitable opportunities.
+          </p>
+          <div className="mt-[30px] flex lg:flex-row flex-col gap-[20px]">
+            <button className=" lg:w-[170px] border border-tradeGreen py-[7px] rounded-[8px] text-white bg-tradeGreen hover:bg-transparent transition-all duration-300 font-[600]">
+              Get Started Now
+            </button>
+            <button className="lg:w-[230px] border border-tradeOrange py-[7px] rounded-[8px]  text-white hover:bg-tradeOrange hover:text-black transition-all duration-300 font-[600]">
+              Explore our Marketplace
+            </button>
+          </div>
         </div>
+        <div className="flex-1 hidden lg:flex">hh</div>
       </div>
     </div>
   );
