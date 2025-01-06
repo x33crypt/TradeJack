8b175f474fa352c7be8db81cf967553975cc6b71
br3nst3rm <adeleke21l4l@gmail.com>
2025-01-06 02:02:55 +0100

    hel

--- src/components/landingPage/TopLayer.jsx
+++ src/components/landingPage/TopLayer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import trading from "../../assets/trading.svg";
 
 const TopLayer = () => {
   return (
@@ -14,15 +15,17 @@ const TopLayer = () => {
             trades and profitable opportunities.
           </p>
           <div className="mt-[30px] flex lg:flex-row flex-col gap-[20px]">
-            <button className=" lg:w-[170px] border border-tradeGreen py-[7px] rounded-[8px] text-white bg-tradeGreen hover:bg-transparent transition-all duration-300 font-[600]">
+            <button className=" lg:w-[170px] border border-tradeGreen py-[8px] rounded-[10px] text-white bg-tradeGreen hover:bg-transparent transition-all duration-300 font-[600]">
               Get Started Now
             </button>
-            <button className="lg:w-[230px] border border-tradeOrange py-[7px] rounded-[8px]  text-white hover:bg-tradeOrange hover:text-black transition-all duration-300 font-[600]">
+            <button className="lg:w-[230px] border border-tradeOrange py-[10px] rounded-[8px]  text-white hover:bg-tradeOrange hover:text-black transition-all duration-300 font-[600]">
               Explore our Marketplace
             </button>
           </div>
         </div>
-        <div className="flex-1 hidden lg:flex">hh</div>
+        <div className="flex-1 hidden lg:flex justify-center">
+          <img className="w-[90%]" src={trading} alt="" />
+        </div>
       </div>
     </div>
   );
