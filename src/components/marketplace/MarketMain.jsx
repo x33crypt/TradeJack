f9c890c3d022f505442b289a4a8f813f9afa18df
br3nst3rm <adeleke21l4l@gmail.com>
2025-02-20 21:55:33 +0100

    hello

--- src/components/marketplace/MarketMain.jsx
+++ src/components/marketplace/MarketMain.jsx
@@ -65,9 +65,9 @@ const MarketMain = () => {
 
   return (
     <div className="flex flex-col gap-[10px] bg-tradeAs rounded-[8px] bg- ">
-      <div className="lg:p-[10px] p-[8px] flex flex-col rounded-[8px] bg-  ">
+      <div className="lg:py-[10px] py-[8px] flex flex-col rounded-[8px] bg- gap-[8px]  ">
         <p className="text-white lg:text-[28px] md:text-[25px] sm:text-[25px] text-[25px] font-[900]">
-          Secure Asset Marketplace
+          Marketplace
         </p>
         <p className="text-tradeFadeWhite lg:text-[15px] sm:text-[14px] text-[14px]">
           Sell a variety of digital assets to buyers worldwide with ease,
