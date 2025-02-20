6dbb221bae9a2641841a423733ae9f347dc1f798
br3nst3rm <adeleke21l4l@gmail.com>
2025-02-20 23:04:22 +0100

    hello

--- src/components/marketplace/MarketMain.jsx
+++ src/components/marketplace/MarketMain.jsx
@@ -76,7 +76,7 @@ const MarketMain = () => {
         </p>
       </div>
 
-      <div className=" px-[10px] py-[6px] bg-tradeAshLight border border-neutral-700 rounded-[3px] ">
+      <div className=" px-[10px] py-[7px] bg-tradeAshLight border border-neutral-700 rounded-[3px] ">
         <div className="flex justify-between items-center cursor-pointer">
           <p className="text-white font-[700]">Filter Offers</p>
           <div className="p-[6px] border border-white rounded-[3px]">
@@ -86,7 +86,7 @@ const MarketMain = () => {
       </div>
 
       <div className="flex flex-col lg:gap-[10px] gap-[4px] bg-tradeAsh borde border-neutral-800 rounded-[8px] lg:p-[10px] p-[5px]">
-        <div className="bg-tradeOrange lg:px-[20px] px-[10px] py-[5px] rounded-[5px]">
+        <div className="bg-tradeOrange lg:px-[20px] px-[10px] py-[7px] rounded-[5px]">
           <p className="font-[600] text-[15px]">Promoted Offers</p>
         </div>
         <div className="grid grid-cols-1 lg:gap-[10px] gap-[4px] items-center">
@@ -108,7 +108,7 @@ const MarketMain = () => {
         </div>
       </div>
       <div className="flex flex-col lg:gap-[10px] gap-[4px] bg-tradeAsh borde border-neutral-800 rounded-[8px] lg:p-[10px] p-[5px]">
-        <div className="bg-[rgb(231,206,109)] lg:px-[20px] px-[10px] py-[5px] rounded-[5px]">
+        <div className="bg-[rgb(231,206,109)] lg:px-[20px] px-[10px] py-[7px] rounded-[5px]">
           <p className="font-[600] text-[15px]">Other Offers</p>
         </div>
         <div className="grid grid-cols-1 lg:gap-[10px] gap-[4px] items-center">
