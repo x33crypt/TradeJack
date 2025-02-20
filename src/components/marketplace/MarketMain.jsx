754ed992bbf25d59782fe23070a882f1fa8dba41
br3nst3rm <adeleke21l4l@gmail.com>
2025-02-20 23:01:14 +0100

    hello

--- src/components/marketplace/MarketMain.jsx
+++ src/components/marketplace/MarketMain.jsx
@@ -76,18 +76,18 @@ const MarketMain = () => {
         </p>
       </div>
 
-      <div className=" px-[10px] py-[8px] bg-tradeAshLight border border-neutral-700 rounded-[3px] ">
+      <div className=" px-[10px] py-[6px] bg-tradeAshLight border border-neutral-700 rounded-[3px] ">
         <div className="flex justify-between items-center cursor-pointer">
           <p className="text-white font-[700]">Filter Offers</p>
           <div className="p-[6px] border border-white rounded-[3px]">
-            <IoFilter className="text-tradeFadeWhite text-[20px]" />
+            <IoFilter className="text-tradeFadeWhite text-[18px]" />
           </div>
         </div>
       </div>
 
-      <div className="flex flex-col lg:gap-[10px] gap-[4px] bg-tradeAsh borde border-neutral-800 rounded-[8px] lg:p-[10px] p-[8px]">
+      <div className="flex flex-col lg:gap-[10px] gap-[4px] bg-tradeAsh borde border-neutral-800 rounded-[8px] lg:p-[10px] p-[5px]">
         <div className="bg-tradeOrange lg:px-[20px] px-[10px] py-[5px] rounded-[5px]">
-          <p className="font-[600] text-[14px]">Promoted Offers</p>
+          <p className="font-[600] text-[15px]">Promoted Offers</p>
         </div>
         <div className="grid grid-cols-1 lg:gap-[10px] gap-[4px] items-center">
           {promotedOffers?.map((offer, index) => (
@@ -107,9 +107,9 @@ const MarketMain = () => {
           ))}
         </div>
       </div>
-      <div className="flex flex-col lg:gap-[10px] gap-[4px] bg-tradeAsh borde border-neutral-800 rounded-[8px] lg:p-[10px] p-[8px]">
+      <div className="flex flex-col lg:gap-[10px] gap-[4px] bg-tradeAsh borde border-neutral-800 rounded-[8px] lg:p-[10px] p-[5px]">
         <div className="bg-[rgb(231,206,109)] lg:px-[20px] px-[10px] py-[5px] rounded-[5px]">
-          <p className="font-[600] text-[14px]">Other Offers</p>
+          <p className="font-[600] text-[15px]">Other Offers</p>
         </div>
         <div className="grid grid-cols-1 lg:gap-[10px] gap-[4px] items-center">
           {unPromotedOffers?.map((offer, index) => (
