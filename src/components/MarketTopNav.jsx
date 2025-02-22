8e31757ddd950983b04caa26c2ce66670c1913b9
br3nst3rm <adeleke21l4l@gmail.com>
2025-02-22 13:35:35 +0100

    hello

--- src/components/MarketTopNav.jsx
+++ src/components/MarketTopNav.jsx
@@ -50,16 +50,16 @@ const MarketTopNav = () => {
   const navigateTo = useNavigate();
 
   return (
-    <div className="z-50 fixed right-0 left-0 bg-black lg:px-[1.5%] px-[3%] md:h-[70px] h-[65px] flex justify-between items-center border-b border-neutral-800 ">
+    <div className="z-50 fixed right-0 left-0 bg-black lg:px-[1.5%] px-[3%] h-[70px] flex justify-between items-center border-b border-neutral-800 ">
       <div className="flex items-center lg:gap-[20px] gap-[15px]">
         <div className="lg:hidden flex">
           <HiOutlineMenuAlt2 className="text-white text-[25px]" />
         </div>
         <div className="flex items-center justify-start gap-[5px] ">
-          <GiCardExchange className="md:text-[22px] text-[21px] text-tradeGreen" />
-          <p className=" md:text-[22px] text-[21px] font-[700] text-tradeGreen">
+          <GiCardExchange className="lg:text-[22px] text-[22px] text-tradeGreen" />
+          <p className=" lg:text-[22px] text-[22px] font-[700] text-tradeGreen">
             Trade
-            <small className="md:text-[22px] text-[21px] font-[700] text-white">
+            <small className="lg:text-[22px] text-[22px] font-[700] text-white">
               Jack
             </small>
           </p>
@@ -100,17 +100,17 @@ const MarketTopNav = () => {
           />
         </div>
         <div className="cursor-pointer sm:hidden">
-          <FiSearch className="text-white hover:text-tradeGreen text-[22px] transition-all duration-300" />
+          <FiSearch className="text-white hover:text-tradeGreen text-[24px] transition-all duration-300" />
         </div>
         <div className="cursor-pointer">
-          <FaRegBell className="text-white hover:text-tradeGreen lg:text-[24px] md:text-[23px] text-[22px] transition-all duration-300" />
+          <FaRegBell className="text-white hover:text-tradeGreen sm:text-[24px] text-[24px] transition-all duration-300" />
         </div>
         <div
           className="cursor-pointer sm:p-[5px] p-[4px] bg-tradeAshLight hover:bg-tradeAshExtraLight rounded-full transition-all duration-300"
           onClick={() => setIsNavOption((prev) => !prev)}
         >
           <img
-            className="lg:w-[32px] sm:w-[30px] w-[28px] rounded-full"
+            className="lg:w-[32px] sm:w-[30px] w-[30px] rounded-full"
             src={landingImg4}
             alt=""
           />
