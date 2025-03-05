4d9614275ceeebe7aae42f2f2c0c6b05d81dee03
br3nst3rm <adeleke21l4l@gmail.com>
2025-03-05 21:12:18 +0100

    hell

--- src/components/marketplace/MarketMain.jsx
+++ src/components/marketplace/MarketMain.jsx
@@ -84,8 +84,8 @@ const MarketMain = () => {
           Seamless Asset Trading
         </p>
       </div>
-      <div className="flex flex-col gap-[20px] px-[20px]">
-        <div className="sticky top-[67px] py-[20px] bg-tradeAsh flex justify-between">
+      <div className="flex flex-col md:gap-[10px] gap-[20px] px-[20px]">
+        <div className="z-10 sticky  top-[65px] md:py-[20px] py-[16px] bg-tradeAsh flex justify-between">
           <div className="max-w-max flex items-center gap-[5px] bg-transparent borde border-neutral-800 rounded-[6.5px]">
             <p className="px-[12px] py-[4px] text-[14px] font-[500] rounded-[6.5px] text-neutral-500 hover:text-white hover:bg-tradeAshExtraLight border border-neutral-800 hover:border-tradeAshExtraLight cursor-pointer duration-300 transition-all">
               All
@@ -105,85 +105,89 @@ const MarketMain = () => {
 
         <div className="flex flex-col gap-[20px]">
           <div className="flex flex-col gap-[20px]">
-            <div className="  flex bg-tradeOrange px-[12px] py-[4px] lg:rounded-[6.5px] rounded-[4px]">
+            <div className=" sticky md:top-[135px] top-[127px] flex bg-tradeOrange px-[12px] py-[4px] lg:rounded-[0px] rounded-[0px]">
               <p className="flex-1 text-black text-[14px] font-[600] ">
                 Promoted Offers
               </p>
             </div>
-            <div className=" md:flex hidden bg-tradeAshLight px-[12px] py-[4px] rounded-[6.5px]">
-              <p className="flex-1 text-neutral-400 text-[14px] ">Vendor</p>
-              <p className="flex-1 text-neutral-400 text-[14px]">Details</p>
-              <p className="flex-1 text-neutral-400 text-[14px]">
-                Rate & Offer Performance
-              </p>
-            </div>
-            <div className="grid grid-cols-1 items-center">
-              {promotedOffers?.map((offer, index) => (
-                <div key={index}>
-                  <OfferCard
-                    offerId={offer.offerId}
-                    isVerified={offer.isVerified}
-                    username={offer.username}
-                    lastSeen={offer.lastSeen}
-                    service={offer.service}
-                    serviceType={offer.serviceType}
-                    miniPurchase={offer.miniPurchase}
-                    maxPurchase={offer.maxPurchase}
-                    positiveFeedback={offer.positiveFeedback}
-                    trustScore={offer.trustScore}
-                    currency={offer.currency}
-                    avgTradeTime={offer.avgTradeTime}
-                  />
-                  <div
-                    className={`${
-                      index < promotedOffers.length - 1
-                        ? "border-tradeAshLight"
-                        : "border-transparent"
-                    } border-b`}
-                  ></div>
-                </div>
-              ))}
+            <div className="flex flex-col gap-[20px]">
+              <div className=" md:flex hidden bg-tradeAshLight px-[12px] py-[4px] rounded-[0px]">
+                <p className="flex-1 text-neutral-400 text-[14px] ">Vendor</p>
+                <p className="flex-1 text-neutral-400 text-[14px]">Details</p>
+                <p className="flex-1 text-neutral-400 text-[14px]">
+                  Rate & Offer Performance
+                </p>
+              </div>
+              <div className="grid grid-cols-1 items-center">
+                {promotedOffers?.map((offer, index) => (
+                  <div key={index}>
+                    <OfferCard
+                      offerId={offer.offerId}
+                      isVerified={offer.isVerified}
+                      username={offer.username}
+                      lastSeen={offer.lastSeen}
+                      service={offer.service}
+                      serviceType={offer.serviceType}
+                      miniPurchase={offer.miniPurchase}
+                      maxPurchase={offer.maxPurchase}
+                      positiveFeedback={offer.positiveFeedback}
+                      trustScore={offer.trustScore}
+                      currency={offer.currency}
+                      avgTradeTime={offer.avgTradeTime}
+                    />
+                    <div
+                      className={`${
+                        index < promotedOffers.length - 1
+                          ? "border-tradeAshLight"
+                          : "border-transparent"
+                      } border-b`}
+                    ></div>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
           <div className="flex flex-col gap-[20px]">
-            <div className="flex  bg-white px-[12px] py-[4px] lg:rounded-[6.5px] rounded-[4px]">
+            <div className="sticky md:top-[135px] top-[127px] flex bg-white px-[12px] py-[4px] lg:rounded-[0px] rounded-[0px]">
               <p className="flex-1 text-black text-[14px] font-[600] ">
                 Other Offers
               </p>
             </div>
-            <div className=" md:flex hidden bg-tradeAshLight px-[12px] py-[4px] rounded-[6.5px]">
-              <p className="flex-1 text-neutral-400 text-[14px] ">Vendor</p>
-              <p className="flex-1 text-neutral-400 text-[14px]">Details</p>
-              <p className="flex-1 text-neutral-400 text-[14px]">
-                Rate & Offer Performance
-              </p>
-            </div>
-            <div className="grid grid-cols-1 items-center">
-              {unPromotedOffers?.map((offer, index) => (
-                <div key={index}>
-                  <OfferCard
-                    offerId={offer.offerId}
-                    isVerified={offer.isVerified}
-                    username={offer.username}
-                    lastSeen={offer.lastSeen}
-                    service={offer.service}
-                    serviceType={offer.serviceType}
-                    miniPurchase={offer.miniPurchase}
-                    maxPurchase={offer.maxPurchase}
-                    positiveFeedback={offer.positiveFeedback}
-                    trustScore={offer.trustScore}
-                    currency={offer.currency}
-                    avgTradeTime={offer.avgTradeTime}
-                  />
-                  <div
-                    className={`${
-                      index < promotedOffers.length
-                        ? "border-tradeAshLight"
-                        : "border-transparent"
-                    } border-b`}
-                  ></div>
-                </div>
-              ))}
+            <div className="flex flex-col gap-[20px]">
+              <div className=" md:flex hidden bg-tradeAshLight px-[12px] py-[4px] rounded-[0px]">
+                <p className="flex-1 text-neutral-400 text-[14px] ">Vendor</p>
+                <p className="flex-1 text-neutral-400 text-[14px]">Details</p>
+                <p className="flex-1 text-neutral-400 text-[14px]">
+                  Rate & Offer Performance
+                </p>
+              </div>
+              <div className="grid grid-cols-1 items-center">
+                {unPromotedOffers?.map((offer, index) => (
+                  <div key={index}>
+                    <OfferCard
+                      offerId={offer.offerId}
+                      isVerified={offer.isVerified}
+                      username={offer.username}
+                      lastSeen={offer.lastSeen}
+                      service={offer.service}
+                      serviceType={offer.serviceType}
+                      miniPurchase={offer.miniPurchase}
+                      maxPurchase={offer.maxPurchase}
+                      positiveFeedback={offer.positiveFeedback}
+                      trustScore={offer.trustScore}
+                      currency={offer.currency}
+                      avgTradeTime={offer.avgTradeTime}
+                    />
+                    <div
+                      className={`${
+                        index < promotedOffers.length
+                          ? "border-tradeAshLight"
+                          : "border-transparent"
+                      } border-b`}
+                    ></div>
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
         </div>
