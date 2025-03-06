93df14a338544992b8f03e02ae236c64737400a7
br3nst3rm <adeleke21l4l@gmail.com>
2025-03-06 01:24:02 +0100

    Po

--- src/components/marketplace/OfferFilter.jsx
+++ src/components/marketplace/OfferFilter.jsx
@@ -427,7 +427,7 @@ const OfferFilter = ({
   // console.log(selectedCurrency);
 
   return (
-    <div className="lg:bg-tradeAsh bg-tradeAshLight lg:sticky  w-full lg:h-[495px] h-full lg:top-[80px] flex flex-col rounded-[12px] md:border border-neutral-800 ">
+    <div className="bg-tradeAsh lg:sticky  w-full lg:h-[495px] h-full lg:top-[80px] flex flex-col rounded-[12px] md:border border-neutral-800 ">
       <div className="flex justify-between items-center lg:p-[20px] lg:py-[20px] py-[16px] px-[20px] md:p-[20px] border-b border-neutral-800 ">
         <p className="text-[18px] text-white font-[700] cursor-pointer">
           Filter Offer
