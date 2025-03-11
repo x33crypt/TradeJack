8fd15e1ecf3032bcb938fa7a73cb76ddb172e4ab
br3nst3rm <adeleke21l4l@gmail.com>
2025-03-11 12:01:06 +0100

    Hello

--- src/components/dashboard/DashTopNav.jsx
+++ src/components/dashboard/DashTopNav.jsx
@@ -16,6 +16,7 @@ import { BsLightningCharge } from "react-icons/bs";
 import { TbHelpCircle } from "react-icons/tb";
 import { MdOutlineCancel } from "react-icons/md";
 import { IoCloseSharp } from "react-icons/io5";
+import { FaVenusDouble } from "react-icons/fa";
 
 const DashTopNav = () => {
   const [isNavOption, setIsNavOption] = useState(false);
@@ -157,7 +158,7 @@ const DashTopNav = () => {
           isNavOption ? "flex" : "hidden"
         } z-50 fixed right-0 left-0 top-[67px] bottom-0 bg-black p-[15px] pt-[14px] pb-[16px] lg:hidden flex flex-col gap-[20px] `}
       >
-        <div className="w-full h-max flex flex-co gap-[10px]">
+        <div className="w-full h-max flex flex-col gap-[10px]">
           <p
             onClick={() => {
               navigateTo("/marketplace");
@@ -165,7 +166,7 @@ const DashTopNav = () => {
             }}
             className="flex-1 flex items-center justify-center text-black text-[15px] font-[600] bg-white py-[10px] rounded-[12px]"
           >
-            Exchange Asset
+            Sell Asset
           </p>
           <p
             onClick={() => {
@@ -174,7 +175,7 @@ const DashTopNav = () => {
             }}
             className="flex-1 flex items-center justify-center text-black text-[15px] font-[600] bg-tradeGreen py-[10px] rounded-[12px]"
           >
-            Create Offer
+            Buy Asset
           </p>
         </div>
         <div className="flex flex-col gap-[10px]">
@@ -190,23 +191,23 @@ const DashTopNav = () => {
           </div>
           <div className=" flex gap-[15px] items-center p-[10px] border border-transparent hover:border-tradeAshExtraLight hover:bg-tradeAsh rounded-[12px] ">
             <TbLayoutList className="text-[15px] text-tradeFadeWhite" />
-            <p className="text-white text-[15px] ">Offers</p>
-          </div>
-          <div className=" flex gap-[15px] items-center p-[10px] border border-transparent hover:border-tradeAshExtraLight hover:bg-tradeAsh rounded-[12px] ">
-            <BsLightningCharge className="text-[15px] text-tradeFadeWhite" />
-            <p className="text-white text-[15px] ">Favourite Vendors</p>
+            <p className="text-white text-[15px] ">My Offers</p>
           </div>
           <div className=" flex gap-[15px] items-center p-[10px] border border-transparent hover:border-tradeAshExtraLight hover:bg-tradeAsh rounded-[12px] ">
             <FaRegEnvelope className="text-[15px] text-tradeFadeWhite" />
             <p className="text-white text-[15px] "> Messages</p>
           </div>
           <div className=" flex gap-[15px] items-center p-[10px] border border-transparent hover:border-tradeAshExtraLight hover:bg-tradeAsh rounded-[12px] ">
+            <FaVenusDouble className="text-[15px] text-tradeFadeWhite" />
+            <p className="text-white text-[15px] ">Favourite Vendors</p>
+          </div>
+          <div className=" flex gap-[15px] items-center p-[10px] border border-transparent hover:border-tradeAshExtraLight hover:bg-tradeAsh rounded-[12px] ">
             <RiExchangeFundsLine className="text-[15px] text-tradeFadeWhite" />
-            <p className="text-white text-[15px] ">Trade History</p>
+            <p className="text-white text-[15px] ">Transaction History</p>
           </div>
           <div className=" flex gap-[15px] items-center p-[10px] border border-transparent hover:border-tradeAshExtraLight hover:bg-tradeAsh rounded-[12px] ">
             <MdOutlineQueryStats className="text-[15px] text-tradeFadeWhite" />
-            <p className="text-white text-[15px] ">Trade Statistics</p>
+            <p className="text-white text-[15px] ">Transaction Statistics</p>
           </div>
           <div className=" flex gap-[15px] items-center p-[10px] border border-transparent hover:border-tradeAshExtraLight hover:bg-tradeAsh rounded-[12px] ">
             <LiaUserFriendsSolid className="text-[15px] text-tradeFadeWhite" />
