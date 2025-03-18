c66b4d2ea9fc2fec8c84fe11cb2d8ae6fe02e3a9
br3nst3rm <adeleke21l4l@gmail.com>
2025-03-18 15:25:06 +0100

    Hello

--- src/pages/TradeChat.jsx
+++ src/pages/TradeChat.jsx
@@ -16,41 +16,39 @@ const TradeChat = () => {
         <div className="flex-1 flex justify-between flex-col md:border border-tradeAshLight md:rounded-[10px]">
           <div className=" flex justify-between items-center gap-[10px] p-[15px]  border-b border-tradeAshLight ">
             <div className="flex items-center gap-[10px]">
-              <div className="w-[30px]">
+              <div className="w-[35px]">
                 <img className="rounded-full" src={landingImg4} alt="" />
               </div>
 
-              <div className="flex flex-col">
-                <p className=" text-[16px] text-white font-[700] cursor-pointer">
+              <div className="flex gap-[10px]">
+                <p className=" text-[18px] text-white font-[700] cursor-pointer">
                   0xSanityy
                 </p>
-                {/* <div className="flex items-center gap-[2px]">
+                <div className="flex items-center gap-[2px]">
                   <GoDotFill className=" text-[14px] text-tradeGreen" />
                   <p className="text-tradeFadeWhite text-[13px] font-[600]">
                     Online
                   </p>
-                </div> */}
+                </div>
               </div>
             </div>
             <div className=" flex borde border-tradeAshLight p-[5px] rounded-[6px]">
               <SlOptionsVertical className="text-white text-[16px]" />
             </div>
           </div>
-          <div className="bg- px-[15px] h-full overflow-y-auto">
-            hello
-          </div>
-          <div className="flex items-center gap-[10px] p-[15px]">
-            <div className="p-[6px] rounded-full bg-white text-[18px] text-black">
+          <div className="bg- px-[15px] h-full overflow-y-auto">hello</div>
+          <div className="flex items-center gap-[10px] p-[15px] border-t border-tradeAshLight">
+            <div className="p-[6px] rounded-full bg-white text-[20px] text-black cursor-pointer">
               <IoAttach />
             </div>
             <div className="flex-1 flex gap-[20px] bg-tradeAsh border border-tradeAshExtraLight p-[6px] rounded-[10px]">
               <input
-                className="w-full outline-none px-[2px]  py-[2px] text-[14px] text-white placeholder:text-tradeFadeWhite bg-tradeAsh"
+                className="w-full outline-none px-[4px]  py-[2px] text-[15px] text-white placeholder:text-tradeFadeWhite bg-tradeAsh caret-tradeGreen"
                 type="text"
                 placeholder="Write a message..."
               />
             </div>
-            <div className="p-[6px] rounded-full bg-white text-[18px] text-black">
+            <div className="p-[6px] rounded-full bg-white text-[20px] text-black cursor-pointer">
               <TbBrandTelegram />
             </div>
           </div>
