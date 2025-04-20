3c48a23d9ed1d0a131a5938537ba2c147c1e54ea
br3nst3rm <adeleke21l4l@gmail.com>
2025-04-20 15:53:50 +0100

    going

--- src/pages/SignupSuccess.jsx
+++ src/pages/SignupSuccess.jsx
@@ -6,25 +6,22 @@ const SignupSuccess = () => {
   const navigateTo = useNavigate();
   return (
     <div className="bg-black h-screen flex p-[35px] justify-center items-center">
-      <div className="flex flex-col items-center lg:p-[20px] p-[15px] md:gap-[30px] gap-[20px] rounded-[16px] bg-tradeAsh border border-tradeAshExtraLight ">
-        <div className="lg:text-[120px] md:text-[120px] text-[100px] text-tradeGreen">
-          <IoMdCheckmarkCircleOutline />
-        </div>
-
-        <div className="w-full flex flex-col items-center gap-[25px]">
+      <div className="flex flex-col items-center lg:p-[20px] p-[15px] md:gap-[40px] gap-[30px] rounded-[16px] bg-tradeAsh border border-tradeAshExtraLight ">
+        <div className="w-full flex flex-col items-center gap-[10px]">
+          <div className="lg:text-[120px] md:text-[120px] text-[100px] text-tradeGreen">
+            <IoMdCheckmarkCircleOutline />
+          </div>
           <p className="text-tradeFadeWhite text-[14px] sm:w-[250px] text-center font-[500]">
             Your account has been successfully created. Please log in to access
             your dashboard.
           </p>
-          <button
-            onClick={() => navigateTo("/login")}
-            className=" lg:w-[250px] w-full bg-white p-[10px] rounded-[10px] flex justify-center items-center cursor-pointer hover:bg-gray-100 transition"
-          >
-            <p className="text-[14px] font-[700] text-black">
-              Proceed to Login
-            </p>
-          </button>
         </div>
+        <button
+          onClick={() => navigateTo("/login")}
+          className=" lg:w-[250px] w-full bg-white p-[10px] rounded-[10px] flex justify-center items-center cursor-pointer hover:bg-gray-100 transition"
+        >
+          <p className="text-[14px] font-[600] text-black">Proceed to Login</p>
+        </button>
       </div>
     </div>
   );
