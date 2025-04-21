41806e5b880b4bb92987d8a92bcc7a7df73bc7ab
br3nst3rm <adeleke21l4l@gmail.com>
2025-04-21 02:15:21 +0100

    going

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,24 +1,16 @@
 import Footer from "@/components/Footer";
 import MarketTopNav from "@/components/InAppNav";
 import React, { useState, useEffect, useRef } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
 import image from "../assets/landingImg4.JPG";
 import { MdThumbUpAlt } from "react-icons/md";
 import { MdThumbDownAlt } from "react-icons/md";
 import { FaRegStar } from "react-icons/fa";
-import { PiHorseBold } from "react-icons/pi";
-import { HiMiniMagnifyingGlass } from "react-icons/hi2";
-import { CgProfile } from "react-icons/cg";
-import { PiHandDepositBold } from "react-icons/pi";
-import { MdOutlineSecurity } from "react-icons/md";
-import { TbAlignBoxRightTop } from "react-icons/tb";
-import { BiSupport } from "react-icons/bi";
-import { MdLogin } from "react-icons/md";
-import UserProfileNav from "@/components/UserProfileNav";
-import { GoDotFill } from "react-icons/go";
+import { FaLocationDot } from "react-icons/fa6";
+import { MdKeyboardArrowRight } from "react-icons/md";
+import useSafeNavigate from "@/components/SafeNavigation";
 
 const UserProfile = () => {
-  const [isProfileEdit, setIsProfileEdit] = useState(false);
+  const [isAccountVerified, setIsAccountVerified] = useState(false);
   const [isBankAccountEdit, setIsBankAccountEdit] = useState(false);
   const [isOnlineWalletEdit, setIsOnlineWalletEdit] = useState(false);
 
@@ -26,8 +18,7 @@ const UserProfile = () => {
   const bankAccountRef = useRef(null);
   const walletRef = useRef(null);
 
-  const navigateTo = useNavigate();
-  const location = useLocation();
+  const safeNavigate = useSafeNavigate();
 
   useEffect(() => {
     if (isBankAccountEdit) {
@@ -66,45 +57,18 @@ const UserProfile = () => {
 
                   <div className="flex flex-col gap-[3px] ml-1">
                     <div className="flex items-center gap-[5px]">
-                      <p className="lg:text-[13px] sm:text-[13px] text-[13px] font-[500] text-white">
+                      <FaLocationDot className="text-tradeFadeWhite text-[13px]" />
+                      <p className="lg:text-[14px] sm:text-[13px] text-[13px] font-[500] text-white">
                         Nigeria
                       </p>
                     </div>
                     <div className="flex items-center gap-[3px]">
-                      <p className="lg:text-[13px] sm:text-[13px] text-[13px] font-[500] text-tradeFadeWhite">
+                      <p className="lg:text-[14px] sm:text-[13px] text-[13px] font-[500] text-tradeFadeWhite">
                         Joined 3 weeks ago
                       </p>
                     </div>
                   </div>
                 </div>
-
-                {/* <div className="flex flex-col gap-[2px]">
-                  <div className="flex gap-[10px]">
-                    <div className="flex gap-[3px] items-center">
-                      <p className="text-white lg:text-[13px] sm:text-[13px] text-[14px] font-[500]">
-                        #TopTrader
-                      </p>
-                      <PiHorseBold className="text-tradeOrange" />
-                    </div>
-                    <p className="text-tradeGreen lg:text-[13px] sm:text-[13px] text-[14px] font-[500]">
-                      Online
-                    </p>
-                  </div>
-
-                  <div className="flex items-center gap-[3px]">
-                    <div className="flex items-center gap-[5px]">
-                      <p className="lg:text-[13px] sm:text-[13px] text-[14px] font-[500] text-white">
-                        Nigeria
-                      </p>
-                    </div>
-                    <p className="lg:text-[14px] sm:text-[13px] text-[14px] font-[700] text-tradeFadeWhite">
-                      &#x2022;
-                    </p>
-                    <p className="lg:text-[13px] sm:text-[13px] text-[14px] font-[500] text-tradeFadeWhite">
-                      Joined 3 weeks ago
-                    </p>
-                  </div>
-                </div> */}
 
                 <div className="flex gap-[10px] ml-1">
                   <div className=" flex md:flex-col flex-row gap-[5px] lg:py-[5px] lg:px-[10px] py-[4px] px-[6px] bg-tradeAsh border border-tradeAshLight  rounded-[8px]">
@@ -148,16 +112,14 @@ const UserProfile = () => {
             </div>
 
             <div
-              onClick={() => setIsProfileEdit((prev) => !prev)}
               className={`${
-                isProfileEdit
-                  ? "text-black bg-tradeGreen border-tradeAshExtraLight"
-                  : "bg-tradeAsh  border-tradeAshLight hover:text-white text-tradeFadeWhite "
+                isAccountVerified
+                  ? "bg-tradeAsh  border-tradeAshLight hover:text-white text-tradeFadeWhite "
+                  : " text-black bg-tradeGreen border-tradeAshExtraLight"
               } flex items-center justify-center border gap-[10px] lg:px-[20px] lg:py-[6px]  px-[12px] py-[8px] rounded-[7px] cursor-pointer transition-all duration-300`}
             >
               <p className="text-[14px] font-[600]">
-                {" "}
-                {isProfileEdit ? "Save Changes" : "Edit Profile"}
+                {isAccountVerified ? "Verified Account" : "Verify Account"}
               </p>
             </div>
           </div>
@@ -170,132 +132,112 @@ const UserProfile = () => {
                     Personal Information
                   </p>
                 </div>
-                <div className="flex flex-col gap-[20px]">
-                  {isProfileEdit ? (
-                    <div className="flex flex-col  gap-[20px] p-[15px]">
-                      <div className="flex-1 flex flex-col gap-[5px]">
-                        <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
-                          Full name
-                        </p>
-                        <input
-                          className={`h-[40px] px-[10px] py-[4px] text-white font-[600] text-[15px]  rounded-[8px] border border-neutral-700 outline-none transition-all duration-300 ${
-                            isProfileEdit
-                              ? "cursor-text border-tradeAshExtraLight bg-black"
-                              : "cursor-default bg-tradeAsh "
-                          }`}
-                          type="text"
-                          readOnly={!isProfileEdit}
-                        />
-                      </div>
 
-                      <div className="flex-1 flex flex-col gap-[5px]">
-                        <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
-                          Username
-                        </p>
-                        <input
-                          className={`h-[40px] px-[10px] py-[4px] text-white font-[600] text-[15px]  rounded-[8px] border border-neutral-700 outline-none transition-all duration-300 ${
-                            isProfileEdit
-                              ? "cursor-text border-tradeAshExtraLight bg-black"
-                              : "cursor-default bg-tradeAsh "
-                          }`}
-                          type="text"
-                          readOnly={!isProfileEdit}
-                        />
-                      </div>
+                <div className=" flex flex-col p-[15px] gap-[20px] ">
+                  <div className="flex items-center py-[5px] px-[10px] bg-tradeAsh hover:bg-tradeAshLight border border-tradeAshLight rounded-[8px] cursor-pointer transition-all duration-300 ">
+                    <div className="flex-1 flex flex-col gap-[2px]  ">
+                      <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
+                        Full name
+                      </p>
 
-                      <div className="flex-1 flex flex-col gap-[5px]">
-                        <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
-                          Email address
-                        </p>
-                        <input
-                          className={`h-[40px] px-[10px] py-[4px] text-white font-[600] text-[15px]  rounded-[8px] border border-neutral-700 outline-none transition-all duration-300 ${
-                            isProfileEdit
-                              ? "cursor-text border-tradeAshExtraLight bg-black"
-                              : "cursor-default bg-tradeAsh "
-                          }`}
-                          type="text"
-                          readOnly={!isProfileEdit}
-                        />
-                      </div>
-
-                      <div className="flex-1 flex flex-col gap-[5px]">
-                        <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
-                          Phone number
-                        </p>
-                        <input
-                          className={`h-[40px] px-[10px] py-[4px] text-white font-[600] text-[15px]  rounded-[8px] border border-neutral-700 outline-none transition-all duration-300 ${
-                            isProfileEdit
-                              ? "cursor-text border-tradeAshExtraLight bg-black"
-                              : "cursor-default bg-tradeAsh "
-                          }`}
-                          type="text"
-                          readOnly={!isProfileEdit}
-                        />
-                      </div>
+                      <p className="text-white text-[15px] font-[600]">
+                        Adeleke Lukman
+                      </p>
                     </div>
-                  ) : (
-                    <div className=" flex flex-col p-[15px] gap-[20px] ">
-                      <div className="flex-1 flex flex-col gap-[2px] py-[5px] px-[10px] bg-tradeAsh border border-tradeAshLight rounded-[8px] ">
-                        <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
-                          Full name
-                        </p>
-
-                        <p className="text-white text-[15px] font-[600]">
-                          Adeleke Lukman
-                        </p>
-                      </div>
-
-                      <div className="flex-1 flex flex-col gap-[2px] py-[5px] px-[10px] bg-tradeAsh border border-tradeAshLight rounded-[8px] ">
-                        <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
-                          Username
-                        </p>
-
-                        <p className="text-white text-[15px] font-[600]">
-                          sanityy0x
-                        </p>
-                      </div>
-
-                      <div className="flex-1 flex flex-col gap-[2px] py-[5px] px-[10px] bg-tradeAsh border border-tradeAshLight rounded-[8px] ">
-                        <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
-                          Email address
-                        </p>
-
-                        <p className="text-white text-[15px] font-[600]">
-                          sanityy0x@gmail.com
-                        </p>
-                      </div>
-
-                      <div className="flex-1 flex flex-col gap-[2px] py-[5px] px-[10px] bg-tradeAsh border border-tradeAshLight rounded-[8px] ">
-                        <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
-                          Phone number
-                        </p>
-
-                        <p className="text-white text-[15px] font-[600]">
-                          08039921211
-                        </p>
-                      </div>
-
-                      <div className="flex-1 flex flex-col gap-[2px] py-[5px] px-[10px] bg-tradeAsh border border-tradeAshLight rounded-[8px] ">
-                        <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
-                          Address
-                        </p>
-
-                        <p className="text-white text-[15px] font-[600]">
-                          Igwe Orizu Rd, Nnewi, Anambra
-                        </p>
-                      </div>
-
-                      <div className="flex-1 flex flex-col gap-[2px] py-[5px] px-[10px] bg-tradeAsh border border-tradeAshLight rounded-[8px] ">
-                        <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
-                          Country
-                        </p>
-
-                        <p className="text-white text-[15px] font-[600]">
-                          Nigeria
-                        </p>
-                      </div>
+                    <div className="text-white text-[22px]">
+                      <MdKeyboardArrowRight />
                     </div>
-                  )}
+                  </div>
+
+                  <div className="flex items-center py-[5px] px-[10px] bg-tradeAsh hover:bg-tradeAshLight border border-tradeAshLight  rounded-[8px] cursor-pointer transition-all duration-300">
+                    <div className="flex-1 flex flex-col gap-[2px]  ">
+                      <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
+                        Username
+                      </p>
+
+                      <p className="text-white text-[15px] font-[600]">
+                        sanityy0x
+                      </p>
+                    </div>
+                    <div className="text-white text-[22px]">
+                      <MdKeyboardArrowRight />
+                    </div>
+                  </div>
+
+                  <div className="flex items-center py-[5px] px-[10px] bg-tradeAsh hover:bg-tradeAshLight border border-tradeAshLight  rounded-[8px] cursor-pointer transition-all duration-300">
+                    <div className="flex-1 flex flex-col gap-[2px]  ">
+                      <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
+                        Email address
+                      </p>
+
+                      <p className="text-white text-[15px] font-[600]">
+                        sanityy0x@gmail.com
+                      </p>
+                    </div>
+                    <div className="text-white text-[22px]">
+                      <MdKeyboardArrowRight />
+                    </div>
+                  </div>
+
+                  <div className="flex items-center py-[5px] px-[10px] bg-tradeAsh hover:bg-tradeAshLight border border-tradeAshLight  rounded-[8px] cursor-pointer transition-all duration-300">
+                    <div className="flex-1 flex flex-col gap-[2px]  ">
+                      <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
+                        Phone number
+                      </p>
+
+                      <p className="text-white text-[15px] font-[600]">
+                        08039921211
+                      </p>
+                    </div>
+                    <div className="text-white text-[22px]">
+                      <MdKeyboardArrowRight />
+                    </div>
+                  </div>
+
+                  <div className="flex items-center py-[5px] px-[10px] bg-tradeAsh hover:bg-tradeAshLight border border-tradeAshLight  rounded-[8px] cursor-pointer transition-all duration-300">
+                    <div className="flex-1 flex flex-col gap-[2px]  ">
+                      <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
+                        Address
+                      </p>
+
+                      <p className="text-white text-[15px] font-[600]">
+                        Igwe Orizu Rd, Nnewi, Anambra
+                      </p>
+                    </div>
+                    <div className="text-white text-[22px]">
+                      <MdKeyboardArrowRight />
+                    </div>
+                  </div>
+
+                  <div className="flex items-center py-[5px] px-[10px] bg-tradeAsh hover:bg-tradeAshLight border border-tradeAshLight  rounded-[8px] cursor-pointer transition-all duration-300">
+                    <div className="flex-1 flex flex-col gap-[2px]  ">
+                      <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
+                        Country
+                      </p>
+
+                      <p className="text-white text-[15px] font-[600]">
+                        Nigeria
+                      </p>
+                    </div>
+                    <div className="text-white text-[22px]">
+                      <MdKeyboardArrowRight />
+                    </div>
+                  </div>
+
+                  <div className="flex items-center py-[5px] px-[10px] bg-tradeAsh hover:bg-tradeAshLight border border-tradeAshLight  rounded-[8px] cursor-pointer transition-all duration-300">
+                    <div className="flex-1 flex flex-col gap-[2px]  ">
+                      <p className="text-[12.5px] font-[500] text-tradeFadeWhite">
+                        Currency
+                      </p>
+
+                      <p className="text-white text-[15px] font-[600]">
+                        Nigeria Naira
+                      </p>
+                    </div>
+                    <div className="text-white text-[22px]">
+                      <MdKeyboardArrowRight />
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
