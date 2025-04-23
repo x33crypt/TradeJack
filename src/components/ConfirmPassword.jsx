dc6a88a8dac9d08c85de82cbceb394bf3ab189e1
br3nst3rm <adeleke21l4l@gmail.com>
2025-04-23 22:40:25 +0100

    going

--- src/components/ConfirmPassword.jsx
+++ src/components/ConfirmPassword.jsx
@@ -111,8 +111,8 @@ const ConfirmPassword = () => {
   return (
     <>
       {/* <InAppNav /> */}
-      <div className="pt-[60px] flex items-center justify-center min-h-screen bg-black">
-        <div className="bg-tradeGree flex  flex-col gap-[px] md:borde border-neutral-800 ">
+      <div className="pt-[0px] flex items-center justify-center min-h-svh bg-black">
+        <div className="bg-tradeGree flex flex-col gap-[px] md:borde border-neutral-800 ">
           {/* <div className="flex md:hidden justify-cente border-b p-[15px] border-tradeAshLight">
             <p className=" text-[17px] text-white font-[700]">
               Confirm your password
