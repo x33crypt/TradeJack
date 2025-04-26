add84eb6ca20c9322dcef69635fbee4c1fabd66a
br3nst3rm <adeleke21l4l@gmail.com>
2025-04-26 18:43:59 +0100

    going

--- src/pages/ChangeAdress.jsx
+++ src/pages/ChangeAdress.jsx
@@ -254,7 +254,7 @@ const ChangeAdress = () => {
               Change Address
             </p>
           </div>
-          <form className="h-full mt-[50px]" onSubmit={handleSubmitChange}>
+          <form className="h-full mt-[70px]" onSubmit={handleSubmitChange}>
             <div className="w-full h-full flex flex-col lg:py-[50px] md:py-[50px] p-[15px] md:justify-center md:items-center">
               <div className="flex flex-col justify-between md:w-[400px] w-full h-full md:gap-[30px] gap-[30px]">
                 <div className=" flex flex-col w-full gap-[30px]">
