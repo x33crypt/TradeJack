e8a912ab926ae21f02a8f6c8b80d5d51d4afc380
br3nst3rm <adeleke21l4l@gmail.com>
2025-04-27 02:36:10 +0100

    going

--- src/pages/MessageSideNav.jsx
+++ src/pages/MessageSideNav.jsx
@@ -7,46 +7,24 @@ import { BsChatText } from "react-icons/bs";
 const MessageSideNav = () => {
   const navigateTo = useNavigate();
   return (
-    <div className="lg:flex flex-col hidden w-[300px] max-h-svh pt-[60px] gap-[5px] ">
-      <div className="py-[10px] pr-[15px]">
-        <div className="lg:flex hidden items-center bg-tradeAsh border border-tradeAshLight px-[10px] py-[6px] gap-[10px] rounded-[12px]">
+    <div className="lg:flex flex-col hidden w-[300px] max-h-svh pt-[60px]">
+      <div className="py-[13px] pr-[15px]">
+        <div className="lg:flex hidden items-center bg-tradeAsh border border-tradeAshLight px-[13px] py-[4.2px] gap-[10px] rounded-[10px]">
           <FaMagnifyingGlass className="text-neutral-500 lg:text-[15px] text-[18px]" />
           <input
-            className=" bg-transparent outline-none h-[28px] w-[220px]  placeholder:text-tradeFadeWhite text-[12px] text-white"
+            className=" bg-transparent outline-none h-[28px] w-[220px]  placeholder:text-tradeFadeWhite text-[13px] text-white"
             type="text"
             placeholder="Search chats"
           />
         </div>
       </div>
-      <div className="py-[10px] pr-[15px] flex flex-col gap-[30px] overflow-auto custom-scrollbar">
+      <div className="py-[15px] pr-[15px] flex flex-col gap-[30px] overflow-auto custom-scrollbar">
         <div className="flex flex-col gap-[10px]">
           <div className="flex items-center gap-[10px]">
             <BsPinAngle className="text-tradeFadeWhite" />
             <p className="text-tradeFadeWhite text-[14px]">Pinned</p>
           </div>
           <div className="flex flex-col gap-[10px]">
-            <div className="flex flex-col gap-[5px] p-[10px] bg-black hover:bg-tradeAsh cursor-pointer border border-tradeAshLight rounded-[16px]">
-              <div className="flex justify-between ">
-                <p className="text-white text-[14px] font-[500]">Sane</p>
-                <p className="text-white text-[12px] font-[500]">4 day ago</p>
-              </div>
-              <div className="flex flex-col gap-[5px]">
-                <p className="text-tradeFadeWhite text-[12px]">
-                  I want to exchnage 500 USD worth of zelle funds..
-                </p>
-              </div>
-            </div>
-            <div className="flex flex-col gap-[5px] p-[10px] bg-black hover:bg-tradeAsh cursor-pointer border border-tradeAshLight rounded-[16px]">
-              <div className="flex justify-between ">
-                <p className="text-white text-[14px] font-[500]">Sane</p>
-                <p className="text-white text-[12px] font-[500]">4 day ago</p>
-              </div>
-              <div className="flex flex-col gap-[5px]">
-                <p className="text-tradeFadeWhite text-[12px]">
-                  I want to exchnage 500 USD worth of zelle funds..
-                </p>
-              </div>
-            </div>
             <div className="flex flex-col gap-[5px] p-[10px] bg-black hover:bg-tradeAsh cursor-pointer border border-tradeAshLight rounded-[16px]">
               <div className="flex justify-between ">
                 <p className="text-white text-[14px] font-[500]">Sane</p>
