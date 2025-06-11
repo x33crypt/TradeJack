bac46df09369b3be0a1156a2c43748b1f8b51653
br3nst3rm <adeleke21l4l@gmail.com>
2025-06-11 12:07:16 +0100

    new one

--- src/components/toastCards/toastSuccess.jsx
+++ src/components/toastCards/toastSuccess.jsx
@@ -6,7 +6,7 @@ const ToastSuccess = () => {
   const { toast, setToast } = useToast();
 
   return (
-    <div className="fixed flex items-center justify-center top-[20px] left-1/2 transform -translate-x-1/2 z-50 space-y-2 lg:top-auto lg:bottom-[15px] lg:px-[2%] md:px-[2.5%] px-[15px] bg-transparent w-full">
+    <div className="fixed flex items-center justify-center top-[15px] left-1/2 transform -translate-x-1/2 z-50 space-y-2 lg:top-auto lg:bottom-[15px] lg:px-[2%] md:px-[2.5%] px-[15px] bg-transparent w-full">
       {toast.success && (
         <div
           onClick={() =>
@@ -16,10 +16,10 @@ const ToastSuccess = () => {
               successMessage: "",
             }))
           }
-          className="flex items-center gap-3 bg-tradeAsh text-white border border-tradeGreen p-4 rounded-[10px] shadow-md md:w-[350px] w-full text-center text-[13px] font-semibold toast-animate-fadeInOut"
+          className="flex items-center gap-2 bg-tradeAsh text-white border border-tradeGreen p-4 rounded-[10px] shadow-md md:w-[350px] w-full text-center text-[13px] font-semibold toast-animate-fadeInOut"
         >
           <div>
-            <IoCheckmarkDoneCircleSharp className="text-[20px] text-tradeGreen" />
+            <IoCheckmarkDoneCircleSharp className="text-[18px] text-tradeGreen" />
           </div>
           <p>{toast.successMessage}</p>
         </div>
